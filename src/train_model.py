6925d8c8be59d1fdaf5a43c806650c5299746b64
ChrisRawstone <dr.rawstone@outlook.com>
2024-01-11 11:49:18 +0100

    adding evaluation to training

--- src/train_model.py
+++ src/train_model.py
@@ -1,13 +1,21 @@
-
 from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, Trainer, TrainingArguments
-from datasets import load_dataset, load_metric
+from datasets import load_dataset, load_metric, load_from_disk
 import torch
 import numpy as np
 from accelerate import Accelerator  # Import the Accelerator class
 # import hydra
 
+metric = load_metric("accuracy")
 
-#@hydra.main(config_path="conf", config_name="config")
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    accuracy = metric.compute(predictions=predictions, references=labels)
+    print(f"Accuracy: {accuracy['accuracy']}")
+    return accuracy
+
+
+
 
 def main():
     
@@ -16,23 +24,13 @@ def main():
     device = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
     print(f'Using device: {device}')
 
-    # Load the tokenizer
-    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 
-    # Function to tokenize the input texts
-    def tokenize_and_format(examples):
-        # Tokenize the inputs and add the labels
-        tokenized_inputs = tokenizer(examples['text'], padding='max_length', truncation=True)
-        tokenized_inputs['labels'] = examples['generated']
-        return tokenized_inputs
 
-    # Load datasets
-    train_dataset = load_dataset('csv', data_files='data/processed/train.csv')['train']
-    test_dataset = load_dataset('csv', data_files='data/processed/test.csv')['train']
+    # Load only 100 rows of data from the CSV files
+    train_dataset = load_from_disk('data/processed/train_dataset_tokenized')
+    test_dataset = load_from_disk('data/processed/test_dataset_tokenized')
 
-    # Tokenize datasets
-    train_dataset = train_dataset.map(tokenize_and_format, batched=True)
-    test_dataset = test_dataset.map(tokenize_and_format, batched=True)
+
 
     # Load the model
     model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=2)
@@ -43,13 +41,12 @@ def main():
    
     training_args = TrainingArguments(
         output_dir='./results',
-        num_train_epochs=3,
-        per_device_train_batch_size=16,
+        num_train_epochs=1,
+        per_device_train_batch_size=4,
         per_device_eval_batch_size=64,
-        warmup_steps=500,
         weight_decay=0.01,
         logging_dir='./logs',
-        logging_steps=10,
+        logging_steps=50,
     )
 
     # Define the Trainer
@@ -57,7 +54,8 @@ def main():
         model=model,
         args=training_args,
         train_dataset=train_dataset,
-        eval_dataset=test_dataset
+        eval_dataset=test_dataset,
+        compute_metrics=compute_metrics
     )
 
     # Train the model
@@ -68,11 +66,6 @@ def main():
 
 
 
-
-
-
 if __name__ == '__main__':
-
     main()
 
-    
