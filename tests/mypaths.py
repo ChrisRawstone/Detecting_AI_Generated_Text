3a9dc59da38060014b1fcd5e4055262235b97264
JasminThari <s204155@student.dtu.dk>
2024-01-14 13:33:37 +0100

    m

--- tests/mypaths.py
+++ tests/mypaths.py
@@ -2,8 +2,4 @@ import os
 
 TEST_ROOT = os.path.dirname(__file__)  # root of test folder
 PROJECT_ROOT = os.path.dirname(TEST_ROOT)  # root of project
-PATH_DATAimport os
-
-TEST_ROOT = os.path.dirname(__file__)  # root of test folder
-PROJECT_ROOT = os.path.dirname(TEST_ROOT)  # root of project
 PATH_DATA = os.path.join(PROJECT_ROOT, "data")  # root of data folder
