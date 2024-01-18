867d25410486aed6223fd2713e26b1c8fa8fff50
JasminThari <s204155@student.dtu.dk>
2024-01-18 15:31:35 +0100

    last cleaning

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,13 +1,8 @@
-import sys
 import pytest
 import os.path
-import pandas as pd
-import torch
-from transformers import DistilBertForSequenceClassification
 from datasets import load_from_disk
-from mypaths import PROJECT_ROOT
 from src.predict_model import predict
-from src.utils import download_model_from_gcs
+from src.utils import load_model
 
 
 @pytest.mark.skipif(
@@ -16,14 +11,10 @@ from src.utils import download_model_from_gcs
 )
 def test_predict_model():
     # Read the predictions from json file as table
-    download_model_from_gcs("models/", "ai-detection-bucket", "models", "latest")
-    device = torch.device("cpu")
-
-    model = DistilBertForSequenceClassification.from_pretrained(f"models/latest", num_labels=2)
-    model.to(device)
+    model = load_model()
     tokenized_dataset = load_from_disk("data/processed/tokenized_data/small_data/test_dataset_tokenized")
 
-    predictions_dataframe = predict(model, tokenized_dataset, device)
+    predictions_dataframe = predict(model, tokenized_dataset, 'cpu')
 
     assert "text" in predictions_dataframe.columns, "text not found in predictions_dataframe.columns"
     assert "prediction" in predictions_dataframe.columns, "prediction not found in predictions_dataframe.columns"
