2245ae252cd7e75579e14c8017c0efef494e8516
JasminThari <s204155@student.dtu.dk>
2024-01-18 10:10:09 +0100

    corrected the test

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -8,12 +8,31 @@ from datasets import load_from_disk
 from mypaths import PROJECT_ROOT
 from src.predict_model import predict
 
+def download_model_from_gcs(local_download_dir, bucket_name, gcs_path, model_name):
+    from google.cloud import storage
+    import os
+    client = storage.Client()
+    folder_name = f"{gcs_path}/{model_name}"
+    bucket = client.bucket(bucket_name)
+
+    # Create local download directory if it doesn't exist
+    os.makedirs(os.path.join(local_download_dir, model_name), exist_ok=True)
+
+    # Download each file from the GCS folder to the local download directory
+    blobs = bucket.list_blobs(prefix=folder_name)
+    for blob in blobs:
+        local_file_path = os.path.join(local_download_dir, model_name, os.path.basename(blob.name))
+        blob.download_to_filename(local_file_path)
+
+
 @pytest.mark.skipif(
     not os.path.exists(f"data/processed/tokenized_data/small_data/test_dataset_tokenized"),
     reason="Prediction file not found. Run 'python src/predict_model.py' to generate predictions.")
 def test_predict_model():
     # Read the predictions from json file as table
+    download_model_from_gcs("models/", "ai-detection-bucket", "models", "latest")
     device = torch.device("cpu")
+
     model = DistilBertForSequenceClassification.from_pretrained(f"models/latest", num_labels=2)
     model.to(device)
     tokenized_dataset = load_from_disk("data/processed/tokenized_data/small_data/test_dataset_tokenized")
