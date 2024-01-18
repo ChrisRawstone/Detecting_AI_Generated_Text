dfab2a6d5bd908274a781ad3c76b052de94e44ab
JasminThari <s204155@student.dtu.dk>
2024-01-18 11:19:47 +0100

    data drgti

--- src/data_drift.py
+++ src/data_drift.py
@@ -31,56 +31,54 @@ def download_data_from_gcs(local_download_dir, bucket_name, gcs_path, data_name)
         blob.download_to_filename(local_file_path)
 
 # Importing data
-download_data_from_gcs(local_download_dir="Data_GCS", bucket_name="ai-detection-bucket", gcs_path="data", data_name="processed/csv_files")
+download_data_from_gcs(local_download_dir="Data_GCS", bucket_name="ai-detection-bucket", gcs_path="data", data_name="processed/csv_files/data_drift_essays/data_drift_essays.csv")
 download_data_from_gcs(local_download_dir="Data_GCS", bucket_name="ai-detection-bucket", gcs_path="data", data_name="processed/csv_files/medium_data")
 download_data_from_gcs(local_download_dir="Data_GCS", bucket_name="ai-detection-bucket", gcs_path="data", data_name="inference_predictions/predictions_20240117_175237.csv")
 
-
-def read_csv_files_from_directory(directory_path):
-    if os.path.isdir(directory_path):
-        files_in_directory = os.listdir(directory_path)
-        csv_files = [os.path.join(directory_path, file) for file in files_in_directory if file.endswith('.csv')]
-        for csv_file in csv_files:
-            print(csv_file)
-            data_frame = pd.read_csv(csv_file)
-    return data_frame
-
 # Read CSV files from the specified directories
-reference_data = read_csv_files_from_directory("Data_GCS/processed/csv_files/medium_data/train.csv")
-current_data = read_csv_files_from_directory("Data_GCS/processed/csv_files")
-prediction_data = read_csv_files_from_directory("Data_GCS/inference_predictions/predictions_20240117_175237.csv")
+current_data = pd.read_csv("Data_GCS/processed/csv_files/data_drift_essays/data_drift_essays.csv")
+prediction_data = pd.read_csv("Data_GCS/inference_predictions/predictions_20240117_175237.csv")
+reference_data = pd.read_csv("Data_GCS/processed/csv_files/medium_data/train.csv")
 
-current_data = read_csv_files_from_directory("Data_GCS/processed/csv_files")
 
 reference_data["prediction"] = reference_data["label"]
-current_data["prediction"] = current_data["label"]
-
-
-
-# GLoVE embedding
+current_data["prediction"] = current_data["fjeks"]
 column_mapping = ColumnMapping(target="generated", text_features=["text"], prediction="prediction")
 
-# Generating reports
-data_drift_report = Report(metrics=[DataDriftPreset(num_stattest="ks", cat_stattest="psi", num_stattest_threshold=0.2, cat_stattest_threshold=0.2)])
-data_drift_report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
+def data_drift(reference_data, current_data, column_mapping):
+    # Generating reports
+    data_drift_report = Report(metrics=[DataDriftPreset(num_stattest="ks", cat_stattest="psi", num_stattest_threshold=0.2, cat_stattest_threshold=0.2)])
+    data_drift_report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
 
-data_quality_report = Report(metrics=[DataQualityPreset()])
-data_quality_report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
+    data_quality_report = Report(metrics=[DataQualityPreset()])
+    data_quality_report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
 
-target_drift_report = Report(metrics=[TargetDriftPreset()])
-target_drift_report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
+    target_drift_report = Report(metrics=[TargetDriftPreset()])
+    target_drift_report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
+    return data_drift_report, data_quality_report, target_drift_report
 
-classification_report = Report(metrics=[ClassificationPreset()])
-classification_report.run(reference_data=reference_data, current_data=prediction_data, column_mapping=column_mapping)
+def save_reports(data_drift_report, data_quality_report, target_drift_report, column_mapping):
+    # Saving reports
+    base_directory = "src/data_drifting"
+    os.makedirs(base_directory, exist_ok=True)
 
-# Saving reports
-base_directory = "src/data_drifting"
-os.makedirs(base_directory, exist_ok=True)
+    reports = [data_drift_report, data_quality_report, target_drift_report]
+    report_names = ["data_drift_report", "data_quality_report", "target_drift_report"]
+    # Save each report to a separate HTML file
+    for report, name in zip(reports, report_names):
+        html_file_path = os.path.join(base_directory, f"report_{name}.html")
+        report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
+        report.save_html(html_file_path)
 
-reports = [data_drift_report, data_quality_report, target_drift_report, classification_report]
-report_names = ["data_drift_report", "data_quality_report", "target_drift_report", "classification_report"]
-# Save each report to a separate HTML file
-for report, name in zip(reports, report_names):
-    html_file_path = os.path.join(base_directory, f"report_{name}.html")
-    report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
-    report.save_html(html_file_path)
+def classification_report(reference_data, current_data):
+    column_mapping = ColumnMapping(target="generated", text_features=["text"], prediction="prediction")
+    classification_report = Report(metrics=[ClassificationPreset()])
+    classification_report.run(reference_data=reference_data, current_data=prediction_data, column_mapping=column_mapping)
+    html_file_path = os.path.join("src/data_drifting", "report_classification_report.html")
+    classification_report.run(reference_data=reference_data, current_data=current_data, column_mapping=column_mapping)
+    classification_report.save_html(html_file_path)
+
+if __name__ == "__main__":
+    data_drift_report, data_quality_report, target_drift_report = data_drift(reference_data, current_data)
+    save_reports(data_drift_report, data_quality_report, target_drift_report, column_mapping)
+    classification_report(reference_data, prediction_data)
